a66cf9bb8f35e2c7dec9642ee65faecc80842d94
Kenneth Guillén S. <kenneth144rj@yahoo.com>
2025-06-05 11:25:33 -0600

    mangaworld

---
+++ mangaworld1/src/main/java/com/mangaworld/MangaworldApplication.java
@@ -0,0 +1,13 @@
+package com.mangaworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaworldApplication.class, args);
+	}
+
+}
---
+++ mangaworld1/src/test/java/com/mangaworld/MangaworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangaworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
