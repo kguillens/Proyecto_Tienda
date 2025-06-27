5601030f75941c11cbaa00e66e4c70bf070554ea
Kenneth Guillén S. <kenneth144rj@yahoo.com>
2025-06-26 22:06:06 -0600

    adding new features

---
+++ Tienda_v1/src/main/java/com/tienda/controller/CategoriaController.java
@@ -0,0 +1,35 @@
+ @GetMapping("/nuevo")
+    public String categoriaNuevo(Categoria categoria) {
+        return "/categoria/modifica";
+    }
+
+    @Autowired
+    private FirebaseStorageServiceImpl firebaseStorageService;
+    
+    @PostMapping("/guardar")
+    public String categoriaGuardar(Categoria categoria,
+            @RequestParam("imagenFile") MultipartFile imagenFile) {        
+        if (!imagenFile.isEmpty()) {
+            categoriaService.save(categoria);
+            categoria.setRutaImagen(
+                    firebaseStorageService.cargaImagen(
+                            imagenFile, 
+                            "categoria", 
+                            categoria.getIdCategoria()));
+        }
+        categoriaService.save(categoria);
+        return "redirect:/categoria/listado";
+    }
+
+    @GetMapping("/eliminar/{idCategoria}")
+    public String categoriaEliminar(Categoria categoria) {
+        categoriaService.delete(categoria);
+        return "redirect:/categoria/listado";
+    }
+
+    @GetMapping("/modificar/{idCategoria}")
+    public String categoriaModificar(Categoria categoria, Model model) {
+        categoria = categoriaService.getCategoria(categoria);
+        model.addAttribute("categoria", categoria);
+        return "/categoria/modifica";
+    }   
