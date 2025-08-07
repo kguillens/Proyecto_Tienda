7ca383203765cd1e1760668e0cbcd54767a1881e
Kenneth Guillén S. <kenneth144rj@yahoo.com>
2025-08-07 10:09:34 -0600

    Updates plantillas

--- Tienda_v1/src/main/java/com/tienda/service/impl/CorreoServiceImpl.java
+++ Tienda_v1/src/main/java/com/tienda/service/impl/CorreoServiceImpl.java
@@ -3,6 +3,7 @@ package com.tienda.service.impl;
 import com.tienda.service.CorreoService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
  *
  * @author guillen.kenneth
  */
+@Slf4j
 @Service
 public class CorreoServiceImpl implements CorreoService{
     @Autowired
@@ -19,13 +21,22 @@ public class CorreoServiceImpl implements CorreoService{
 
     @Override
     public void enviarCorreoHtml(String para, String asunto, String contenidoHtml) throws MessagingException {
-                MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper 
-                = new MimeMessageHelper(message, 
-                        true);
-        helper.setTo(para);
-        helper.setSubject(asunto);
-        helper.setText(contenidoHtml,true);
-        mailSender.send(message);
+        try {
+            log.info("Intentando enviar correo a: {}", para);
+            
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            
+            helper.setTo(para);
+            helper.setSubject(asunto);
+            helper.setText(contenidoHtml, true);
+            
+            mailSender.send(message);
+            
+            log.info("Correo enviado exitosamente a: {}", para);
+        } catch (MessagingException e) {
+            log.error("Error al enviar correo a {}: {}", para, e.getMessage());
+            throw e;
+        }
     }
 }
